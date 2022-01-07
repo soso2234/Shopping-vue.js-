4127fa3bcedcce10d110158f13a6d9fee974d486
soso2234 <mingi371@naver.com>
2022-01-07 10:00:59 +0900

    commit

---
+++ 제품상세 쿼리.sql
@@ -0,0 +1,63 @@
+SELECT 
+	  t1.*
+	, t2.category1
+	, t2.category2
+	, t2.category3
+	, t3.path
+FROM 
+	  t_product t1
+	, t_category t2
+	, t_image t3
+WHERE 1=1
+AND t1.category_id 	= t2.id
+AND t1.id 				= t3.product_id
+AND t3.type 			= 1
+
+
+SELECT 
+	  t1.*
+	, t2.category1
+	, t2.category2
+	, t2.category3
+	, t3.path
+FROM 
+	  t_product t1
+	, t_category t2
+	, t_image t3
+WHERE 1=1
+AND t1.category_id 	= t2.id
+AND t1.id 				= t3.product_id
+AND t3.type 			= 3
+AND t1.id				= 1
+
+
+SELECT
+	* 
+FROM 
+	t_image
+WHERE 1=1
+AND product_id = 1
+AND type			= 2
+
+INSERT INTO t_product
+(
+	  product_name
+	, product_price
+	, delivery_price
+	, add_delivery_price
+	, tags
+	, outbound_days
+	, seller_id
+	, category_id
+)
+VALUES
+(
+	'테스트 제품'
+	, '50000'
+	, '2500'
+	, '5000't_product
+	, '테스트'
+	, 5
+	, 1
+	, 1
+)

5460d0049ef28483d2c2eab452bcb5fd3e259bd9
soso2234 <mingi371@naver.com>
2022-01-10 18:04:26 +0900

    vue.js

--- 제품상세 쿼리.sql
+++ 제품상세 쿼리.sql
@@ -60,4 +60,9 @@ VALUES
 	, 5
 	, 1
 	, 1
-)
+);
+
+
+select t1.*, t2.path, t3.category1, t3.category2, t3.category3 
+      from t_product t1, t_image t2, t_category t3
+      where t1.id = t2.product_id and t2.type = 1 and t1.category_id = t3.id
